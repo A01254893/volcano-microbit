bce943c1bde380e50037dbaa67bd0e4b9982ff34
A01254893 <112582801+A01254893@users.noreply.github.com>
2022-09-12 10:28:08 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+}
